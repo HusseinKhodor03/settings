3bbaa5acdbc0499fcdb8bc50b06a9d3253845d9e
Hussein Khodor <hkhodor03@gmail.com>
2025-07-15 21:38:15 -0700

    Added code to ensure Homebrew is available in PATH immediately after installation

--- macos/scripts/setup.sh
+++ macos/scripts/setup.sh
@@ -59,6 +59,13 @@ function install_homebrew() {
 	else
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null 2>&1
 	fi
+
+	if [[ -x /opt/homebrew/bin/brew ]]; then
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	elif [[ -x /usr/local/bin/brew ]]; then
+		eval "$(/usr/local/bin/brew shellenv)"
+	fi
+
 	ANY_CHANGES_MADE=true
 }
 
